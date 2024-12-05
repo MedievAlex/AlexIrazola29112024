398a5805ca03178c2a47644191933d583ce0f85a
MedievAlex <alexirazola@gmail.com>
2024-12-05 13:21:42 +0100

    ver.02.01(2024/12/02)

--- src/fichero/Prueba.java
+++ src/fichero/Prueba.java
@@ -3,6 +3,6 @@ package fichero;
 public class FicheroJava {
 
 	public static void main(String[] args) {
-		System.out.println("Este es mi fichero JAVA.");
+		System.out.println("Este es mi fichero JAVA de prueba.");
 	}
 }
