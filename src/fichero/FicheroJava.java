c55d2d5ebec2c5df5d99f8f22b369175b8e58d29
MedievAlex <alexirazola@gmail.com>
2024-12-12 12:30:26 +0100

    ver.03(Añadido una linea de prueba 2024/12/12)

--- src/fichero/FicheroJava.java
+++ src/fichero/FicheroJava.java
@@ -4,5 +4,6 @@ public class FicheroJava {
 
 	public static void main(String[] args) {
 		System.out.println("Este es mi fichero JAVA.");
+		System.out.println("Y este es el cambio que he echo.");
 	}
 }
